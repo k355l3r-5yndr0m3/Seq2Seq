33e9f04545db38985e8fd972661fa2231fb7fc66
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2023-04-14 01:02:23 +0700

    Add utils for saving checkpoints. An empty generator.py file for text decoding algors.

---
+++ utils.py
@@ -0,0 +1,30 @@
+import torch
+
+from torch import nn, optim
+from os import listdir, path, remove
+from fnmatch import fnmatch
+
+def save_check_point(model: nn.Module, optimizer: optim.Optimizer, save_path: str = "checkpoint.pth"):
+    state_dict = {
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+    torch.save(state_dict, save_path)
+
+
+def load_check_point(model: nn.Module, optimizer: optim.Optimizer, load_path: str = "checkpoint.pth"):
+    state_dict = torch.load(load_path)
+    model.load_state_dict(state_dict["model"])
+    optimizer.load_state_dict(state_dict["optimizer"])
+
+
+def keep_n_checkpoints(model: nn.Module, optimizer: optim.Optimizer, checkpoints_dir: str = "checkpoints/", keep_n: int = 4):
+    older_checkpoints = [cp for cp in listdir(checkpoints_dir) if fnmatch(cp, "checkpoint-*.pth")]
+    newest_idx = max([int(cp.split('-')[1].split('.')[0]) for cp in older_checkpoints]) if len(older_checkpoints) > 0 else None
+    if newest_idx:
+        if len(older_checkpoints) >= keep_n:
+            oldest_idx = min([int(cp.split('-')[1].split('.')[0]) for cp in older_checkpoints])
+            remove(path.join(checkpoints_dir, f"checkpoint-{oldest_idx}.pth"))
+        save_check_point(model, optimizer, save_path=path.join(checkpoints_dir, f"checkpoint-{newest_idx+1}.pth"))
+    else:
+        save_check_point(model, optimizer, save_path=path.join(checkpoints_dir, "checkpoint-0.pth"))
