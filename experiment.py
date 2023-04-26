e48fa98a8fd686450f5905f3b6a06cc54408ca46
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2023-04-26 16:44:03 +0000

    FEWERTWE

--- experiment.py
+++ experiment.py
@@ -14,17 +14,18 @@ from generator import testing
 start = 1
 end = 2
 padding = 3
-epoch_num = 512
+epoch_num = 128
 num_checkpoints = 4
 
 d_model = 512
 warmup_step = 4000
-split_max_token = 2**12+2**9
+split_max_token = 2**13
+batch_size = 256
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = Seq2SeqTransformer(padding=padding, device=device, seperate_embedding=True, decople_token_decoder=True, embedding_dim=d_model,
-                           num_encoder_layers = 8)
+                           num_encoder_layers = 8, dropout = 0.1)
 # optimizer = optim.Adadelta(model.parameters())
 optimizer = optim.Adam(model.parameters(), lr=1.0, betas=(0.9, 0.98), eps=1e-9)
 scheduler = lr_scheduler.LambdaLR(optimizer=optimizer, verbose=True,
@@ -47,7 +48,7 @@ with open("train_loss", "a", buffering=1) as graph, open("test_translation", "a"
     for epoch in range(start_epoch_num, epoch_num):
         # training
         dataloader = get_dataloader(Corpus(bidirectional=False), starting_value=start, ending_value=end, padding_value=padding,
-                                    token_limit=split_max_token)
+                                    token_limit=split_max_token, batch_size=batch_size)
         print(f"{'-'*8}{epoch+1}/{epoch_num}{'-'*8}", file=graph)
         train_epoch(dataloader, model, optimizer, criterion, scheduler, on_device=device, loss_take_arg=False, write_loss_to=graph)
         print(f"{'='*32}\n", file=graph)
@@ -60,10 +61,10 @@ with open("train_loss", "a", buffering=1) as graph, open("test_translation", "a"
 
         # validate
         validate_set = get_dataloader(Corpus(validation_set=True, bidirectional=False), starting_value=start, ending_value=end, padding_value=padding,
-                                      token_limit=split_max_token)
+                                      token_limit=split_max_token, batch_size=batch_size)
         keep_n_checkpoints(model, optimizer, scheduler, keep_n=num_checkpoints)
         val = validate(validate_set, model, criterion, device=device)
-        print(f'{epoch+1} {val}', file=vali)
+        print(f'EPOCH:{epoch+1}->{val}', file=vali)
         del validate_set
         if validate_best > val:  # saving best performer
             print("Saving best so far... ", val)
